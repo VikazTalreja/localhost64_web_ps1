447968d91e180e98291c7b03d2f5a889252f186c
Vikas Talreja <vikazztalreja@gmail.com>
2024-01-18 09:29:14 +0530

    intial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-
-import React from 'react';
+import React from "react";
 //import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import LoginForm from './pages/LoginForm';
-import SignUpForm from './pages/SignUpForm';
-import IncidentForm from './pages/IncidentForm';
-import IncidentList from './pages/IncidentList';
+import LoginForm from "./pages/LoginForm";
+import SignUpForm from "./pages/SignUpForm";
+import IncidentForm from "./pages/IncidentForm";
+import IncidentList from "./pages/IncidentList";
 import ClientDashboard from "./pages/ClientDashboard";
 import Attractions from "./pages/Attractions";
 
 function App() {
   return (
-    <>
+    <div className="bg-black">
       {/* <ClientDashboard /> */}
-      <Attractions />
-    </>
+      {/* <Attractions /> */}
+      <SignUpForm />
+      <LoginForm />
+      {/* <LoginForm className="overflow-y-hidden" /> */}
+    </div>
   );
 }
 
